e3882d7ae1f71d22c9aa896321ec3d3532f07f96
Ramkumar Ramachandra <artagnon@gmail.com>
2015-04-25 18:28:56 -0400

    Add toy Cocoa application target

---
+++ UI/Co/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  co
+//
+//  Created by Ramkumar Ramachandra on 4/23/15.
+//  Copyright (c) 2015 Ramkumar Ramachandra. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
