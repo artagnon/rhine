7a11e544ee5018ff6011cfde72a3d0f27cec954d
Ramkumar Ramachandra <artagnon@gmail.com>
2015-04-25 18:28:56 -0400

    Add toy Cocoa application target

---
+++ UI/Co/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  co
+//
+//  Created by Ramkumar Ramachandra on 4/23/15.
+//  Copyright (c) 2015 Ramkumar Ramachandra. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
