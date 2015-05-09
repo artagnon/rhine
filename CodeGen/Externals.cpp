378d70489fd1a874c3c250e99199681d670db109
Ramkumar Ramachandra <artagnon@gmail.com>
2015-05-09 17:16:28 -0400

    Resolve the issue of Externals caching

--- CodeGen/Externals.cpp
+++ CodeGen/Externals.cpp
@@ -9,11 +9,18 @@ using namespace llvm;
 
 namespace rhine {
 llvm::Function *Externals::printf(llvm::Module *M) {
+  // We don't to generate two printf functions (printf, printf1) for the same
+  // module; for different modules, they're different functions
+  static llvm::Module *ThisModule = nullptr;
+  static llvm::Function *Handle = nullptr;
+  if (ThisModule == M && Handle) return Handle;
+  ThisModule = M;
+
   auto ArgTys = llvm::ArrayRef<llvm::Type *>(RhBuilder.getInt8PtrTy());
   llvm::FunctionType* printf_type =
     llvm::FunctionType::get(RhBuilder.getInt32Ty(), ArgTys, true);
 
-  auto Handle =
+  Handle =
     llvm::Function::Create(printf_type, llvm::Function::ExternalLinkage,
                            Twine("printf"), M);
   Handle->setCallingConv(llvm::CallingConv::C);
