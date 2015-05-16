1ea30b1326bd0b511b27a9d1fb8bc8e8ef09b5b9
Ramkumar Ramachandra <artagnon@gmail.com>
2015-05-16 13:40:15 -0400

    Use getOrInsertFunction to get caching for free

--- CodeGen/Externals.cpp
+++ CodeGen/Externals.cpp
@@ -9,38 +9,24 @@ using namespace llvm;
 
 namespace rhine {
 llvm::Function *Externals::printf(llvm::Module *M) {
-  // We don't to generate two printf functions (printf, printf1) for the same
-  // module; for different modules, they're different functions
-  static llvm::Module *ThisModule = nullptr;
-  static llvm::Function *Handle = nullptr;
-  if (ThisModule == M && Handle) return Handle;
-  ThisModule = M;
-
   auto ArgTys = llvm::ArrayRef<llvm::Type *>(RhBuilder.getInt8PtrTy());
   llvm::FunctionType* printf_type =
     llvm::FunctionType::get(RhBuilder.getInt32Ty(), ArgTys, true);
-
-  Handle =
-    llvm::Function::Create(printf_type, llvm::Function::ExternalLinkage,
-                           Twine("printf"), M);
-  Handle->setCallingConv(llvm::CallingConv::C);
-  return Handle;
+  auto Handle =
+    M->getOrInsertFunction("printf", printf_type);
+  if (auto HandleF = dyn_cast<llvm::Function>(Handle))
+    return HandleF;
+  assert(0 && "Prototype previously declared with wrong signature");
 }
 
 llvm::Function *Externals::malloc(llvm::Module *M) {
-  static llvm::Module *ThisModule = nullptr;
-  static llvm::Function *Handle = nullptr;
-  if (ThisModule == M && Handle) return Handle;
-  ThisModule = M;
-
   auto ArgTys = llvm::ArrayRef<llvm::Type *>(RhBuilder.getInt64Ty());
   llvm::FunctionType* malloc_type =
     llvm::FunctionType::get(RhBuilder.getInt8PtrTy(), ArgTys, false);
-
-  Handle =
-    llvm::Function::Create(malloc_type, llvm::Function::ExternalLinkage,
-                           Twine("malloc"), M);
-  Handle->setCallingConv(llvm::CallingConv::C);
-  return Handle;
+  auto Handle =
+    M->getOrInsertFunction("malloc", malloc_type);
+  if (auto HandleF = dyn_cast<llvm::Function>(Handle))
+    return HandleF;
+  assert(0 && "Prototype previously declared with wrong signature");
 }
 }
