7316be0e173cf2d5cb7ac6f466ecef5bf2f9f45b
Ramkumar Ramachandra <artagnon@gmail.com>
2015-06-07 00:47:29 -0400

    Seal off type coercion for int to string via toString

--- unittest/TyCoerceTest.cpp
+++ unittest/TyCoerceTest.cpp
@@ -3,7 +3,7 @@
 
 using namespace rhine;
 
-TEST(TyCoerce, Constant) {
+TEST(TyCoerce, ConstantIntToString) {
   std::string SourcePrg =
     "def main [] {\n"
     "  println 62;\n"
@@ -12,11 +12,22 @@ TEST(TyCoerce, Constant) {
   EXPECT_OUTPUT(SourcePrg, ExpectedOut);
 }
 
-TEST(TyCoerce, Variable) {
+TEST(TyCoerce, StringTyToString) {
   std::string SourcePrg =
-    "def main [] {\n"
-    "  println 62;\n"
-    "}";
-  std::string ExpectedOut = "62";
+    "def boom [var ~String] {\n"
+    "  println var;\n"
+    "}\n"
+    "def main [] boom '12';";
+  std::string ExpectedOut = "12";
+  EXPECT_OUTPUT(SourcePrg, ExpectedOut);
+}
+
+TEST(TyCoerce, IntTyToString) {
+  std::string SourcePrg =
+    "def boom [var ~Int] {\n"
+    "  println var;\n"
+    "}\n"
+    "def main [] boom 3;";
+  std::string ExpectedOut = "3";
   EXPECT_OUTPUT(SourcePrg, ExpectedOut);
 }
