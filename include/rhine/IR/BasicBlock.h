f14a397359d9c1008af2a3be85a0a514b8f2bc97
Ramkumar Ramachandra <artagnon@gmail.com>
2015-06-20 23:07:37 -0400

    Make BasicBlock a Value

--- include/rhine/IR/BasicBlock.h
+++ include/rhine/IR/BasicBlock.h
@@ -6,12 +6,33 @@
 #include "rhine/IR/Value.h"
 
 namespace rhine {
-class BasicBlock {
+class BasicBlock : public Value {
 public:
   std::vector<Value *> ValueList;
-  BasicBlock(std::vector<Value *> V) : ValueList(V) {}
+  BasicBlock(Type *Ty, std::vector<Value *> V) :
+      Value(Ty, RT_BasicBlock), ValueList(V) {}
   static BasicBlock *get(std::vector<Value *> V, Context *K) {
-    return new (K->RhAllocator) BasicBlock(V);
+    return new (K->RhAllocator) BasicBlock(UnType::get(K), V);
+  }
+  static bool classof(const Value *V) {
+    return V->getValID() == RT_BasicBlock;
+  }
+  friend ostream &operator<<(ostream &Stream, const BasicBlock &B) {
+    B.print(Stream);
+    return Stream;
+  }
+  llvm::Constant *toLL(llvm::Module *M, Context *K) { return nullptr; };
+  typedef std::vector<Value *>::iterator iterator;
+  iterator begin() {
+    return ValueList.begin();
+  }
+  iterator end() {
+    return ValueList.end();
+  }
+protected:
+  virtual void print(std::ostream &Stream) const {
+    for (auto V: ValueList)
+      Stream << *V << std::endl;
   }
 };
 }
