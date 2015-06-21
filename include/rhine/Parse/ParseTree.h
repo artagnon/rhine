5f4b2d1b5b69d9bcf2e30ce9eb81648d7a7231fe
Ramkumar Ramachandra <artagnon@gmail.com>
2015-06-21 13:46:21 -0400

    Introduce BasicBlockSpear for unifying IfStmt codepath

--- include/rhine/Parse/ParseTree.h
+++ include/rhine/Parse/ParseTree.h
@@ -13,6 +13,27 @@ class PTree {
 public:
   Module M;
 };
+
+class BasicBlockSpear {
+  Value *Head;
+  std::vector<BasicBlock *> BBList;
+public:
+  BasicBlockSpear(Value *H, std::vector<BasicBlock *> B): Head(H), BBList(B) {}
+  static BasicBlockSpear *get(
+      Value *H, std::vector<BasicBlock *> B, Context *K) {
+    return new (K->RhAllocator) BasicBlockSpear(H, B);
+  }
+  Value *getHead() {
+    return Head;
+  }
+  BasicBlock *getBB(unsigned Idx) {
+    assert(Idx < BBList.size() && "Idx out of BBSpear getBB() bounds");
+    return BBList[Idx];
+  }
+  std::vector <BasicBlock *> getBBs() {
+    return BBList;
+  }
+};
 }
 
 #endif
