e013b507a0118cea9d354b271d31d43da1bd3d64
Ramkumar Ramachandra <artagnon@gmail.com>
2015-07-19 20:34:49 -0400

    Parse lambda more elegantly; no double-semicolon

--- unittest/CodeGenTest.cpp
+++ unittest/CodeGenTest.cpp
@@ -186,24 +186,6 @@ TEST(CodeGen, MultipleArguments)
   EXPECT_PARSE_PP(SourcePrg, ExpectedPP);
 }
 
-TEST(CodeGen, Lambda)
-{
-  std::string SourcePrg =
-    "def foo [] Bfunc = \\x ~ Int -> x;;";
-  std::string ExpectedPP =
-    "define i32 @lambda(i32) {\n"
-    "entry:\n"
-    "  ret i32 %0\n"
-    "}\n\n"
-    "define void @foo() {\n"
-    "entry:\n"
-    "  %BfuncAlloca = alloca i32 (i32)*\n"
-    "  store i32 (i32)* @lambda, i32 (i32)** %BfuncAlloca\n"
-    "  ret void\n"
-    "}";
-  EXPECT_PARSE_PP(SourcePrg, ExpectedPP);
-}
-
 TEST(CodeGen, ArgumentTypesChaining)
 {
   std::string SourcePrg =
