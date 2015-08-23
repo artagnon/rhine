1f9e458c1ab56e47c83103ec54b25790a223a3d2
Ramkumar Ramachandra <artagnon@gmail.com>
2015-08-23 11:46:05 -0400

    Remove dead code

--- include/rhine/Context.h
+++ include/rhine/Context.h
@@ -19,15 +19,7 @@ namespace rhine {
 class Value;
 class Externals;
 
-struct SymbolRef {
-  class Type *Ty;
-  llvm::Value *LLVal;
-  SymbolRef(Type *Typ, llvm::Value *Val) : Ty(Typ), LLVal(Val) {}
-};
-
 class Context {
-  std::map <std::string, SymbolRef> SymbolMapping;
-
 public:
   llvm::BumpPtrAllocator RhAllocator;
   llvm::FoldingSet<FunctionType> FTyCache;
@@ -56,31 +48,6 @@ public:
     RhAllocator.Reset();
     FTyCache.clear();
     ITyCache.clear();
-    SymbolMapping.clear();
-  }
-
-  void addMapping(std::string S, Type *Ty, llvm::Value *V = nullptr) {
-    auto Ret = SymbolMapping.insert(std::make_pair(S, SymbolRef(Ty, V)));
-    if (!Ret.second) {
-      if (Ty) Ret.first->second.Ty = Ty;
-      if (V) Ret.first->second.LLVal = V;
-    }
-  }
-  Type *getMappingTy(std::string S) {
-    auto R = SymbolMapping.find(S);
-    return R == SymbolMapping.end() ? nullptr : R->second.Ty;
-  }
-  llvm::Value *getMappingVal(std::string S) {
-    auto R = SymbolMapping.find(S);
-    return R == SymbolMapping.end() ? nullptr : R->second.LLVal;
-  }
-  llvm::Value *getMappingVal(std::string S, location SourceLoc) {
-    auto R = SymbolMapping.find(S);
-    if (R == SymbolMapping.end() || !R->second.LLVal) {
-      DiagPrinter->errorReport(SourceLoc, "unbound symbol " + S);
-      exit(1);
-    }
-    return R->second.LLVal;
   }
 
   struct ValueRef {
