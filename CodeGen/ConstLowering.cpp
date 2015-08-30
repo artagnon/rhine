82c01d8aa409574d0cb2dbe4f9d9660bc3dd582c
Ramkumar Ramachandra <artagnon@gmail.com>
2015-08-30 14:39:55 -0400

    Towards merging Externals into Resolve

--- CodeGen/ConstLowering.cpp
+++ CodeGen/ConstLowering.cpp
@@ -19,22 +19,36 @@ llvm::Constant *ConstantFloat::toLL(llvm::Module *M) {
   return llvm::ConstantFP::get(LLTy, getVal());
 }
 
+llvm::Function *Prototype::getOrInsert(llvm::Module *M) {
+  auto K = getContext();
+  auto FnTy = cast<llvm::FunctionType>(getType()->toLL(M));
+  auto MangledName = getMangledName();
+
+  // getOrInsertFunction::
+  //
+  // Look up the specified function in the module symbol table.
+  // Four possibilities: 1. If it does not exist, add a prototype for the function
+  // and return it. 2. If it exists, and has a local linkage, the existing
+  // function is renamed and a new one is inserted. 3. Otherwise, if the existing
+  // function has the correct prototype, return the existing function. 4. Finally,
+  // the function exists but has the wrong prototype: return the function with a
+  // constantexpr cast to the right prototype.
+  auto Const = M->getOrInsertFunction(MangledName, FnTy);
+
+  if (auto FunctionCandidate = dyn_cast<llvm::Function>(Const))
+    return FunctionCandidate;
+  auto Error = MangledName + " was declared with different signature earlier";
+  K->DiagPrinter->errorReport(SourceLoc, Error);
+  exit(1);
+}
+
 llvm::Constant *Prototype::toLL(llvm::Module *M) {
-  return Externals::get(getContext())->getMappingVal(Name, M);
+  return getOrInsert(M);
 }
 
 llvm::Constant *Function::toLL(llvm::Module *M) {
   auto K = getContext();
-  auto RhFnTy = cast<FunctionType>(getType());
-  auto FnTy = cast<llvm::FunctionType>(RhFnTy->toLL(M));
-  if (auto FunctionCandidate =
-      dyn_cast<llvm::Function>(M->getOrInsertFunction(Name, FnTy)))
-    K->CurrentFunction = FunctionCandidate;
-  else {
-    K->DiagPrinter->errorReport(
-        SourceLoc, Name + " was declared with different signature earlier");
-    exit(1);
-  }
+  K->CurrentFunction = getOrInsert(M);
   K->CurrentFunction->setGC("rhgc");
 
   // Bind argument symbols to function argument values in symbol table
