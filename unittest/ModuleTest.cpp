7cd94016738aecfc7ee6f67784e19d22e55869da
Ramkumar Ramachandra <artagnon@gmail.com>
2015-09-01 19:33:09 -0400

    Add a couple of Module tests

---
+++ unittest/ModuleTest.cpp
@@ -0,0 +1,31 @@
+#include "rhine/TestUtil.h"
+#include "gtest/gtest.h"
+
+#include "rhine/IR/Module.h"
+
+using namespace rhine;
+
+TEST(Module, CountObjects)
+{
+  std::string SourcePrg =
+    "def main [] {\n"
+    "  print 4;\n"
+    "}\n"
+    "def foom [] 2;\n"
+    "def baz[] print;\n";
+  auto Pf = ParseFacade(SourcePrg);
+  auto Module = Pf.parseToIR(ParseSource::STRING, { });
+  ASSERT_EQ(std::distance(Module->begin(), Module->end()), 3);
+}
+
+TEST(Module, SetFunctionParent)
+{
+  std::string SourcePrg =
+    "def main [] {\n"
+    "  print 4;\n"
+    "}";
+  auto Pf = ParseFacade(SourcePrg);
+  auto Module = Pf.parseToIR(ParseSource::STRING, { });
+  for (auto F : *Module)
+    ASSERT_TRUE(F->getParent() == Module);
+}
