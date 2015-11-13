2084aa373cba867ffcfa32d9235fd0ed7f0557ad
Ramkumar Ramachandra <artagnon@gmail.com>
2015-11-12 20:53:20 -0500

    Use uniqe_ptr in UI/CLI

--- src/UI/CLI/Rhine.cpp
+++ src/UI/CLI/Rhine.cpp
@@ -5,39 +5,34 @@
 
 using namespace rhine;
 
-enum OptionIndex {
-  UNKNOWN,
-  DEBUG,
-  STDIN,
-  HELP
-};
+enum OptionIndex { UNKNOWN, DEBUG, STDIN, HELP };
 
-const option::Descriptor Usage[] =
-  {
+const option::Descriptor Usage[] = {
     {UNKNOWN, 0, "", "", option::Arg::None,
      "USAGE: Rhine [options] <filename>\n\nOptions:"},
     {DEBUG, 0, "", "debug", option::Arg::None,
      " --debug  \tDebug lexer and parser"},
     {STDIN, 0, "", "stdin", option::Arg::None,
      " --stdin  \tRead input from stdin"},
-    {HELP, 0, "", "help", option::Arg::None,
-     " --help  \tPrint usage and exit"},
-    {}
-  };
+    {HELP, 0, "", "help", option::Arg::None, " --help  \tPrint usage and exit"},
+    {}};
 
 int main(int argc, char *argv[]) {
-  argc-=(argc>0); argv+=(argc>0); // skip program name argv[0] if present
-  option::Stats  Stats(Usage, argc, argv);
-  option::Option* Options = new option::Option[Stats.options_max];
-  option::Option* Buffer  = new option::Option[Stats.buffer_max];
-  option::Parser Parse(Usage, argc, argv, Options, Buffer);
+  argc -= (argc > 0);
+  argv += (argc > 0); // skip program name argv[0] if present
+  option::Stats Stats(Usage, argc, argv);
+  auto Options =
+      std::unique_ptr<option::Option[]>(new option::Option[Stats.options_max]);
+  auto Buffer =
+      std::unique_ptr<option::Option[]>(new option::Option[Stats.buffer_max]);
+  option::Parser Parse(Usage, argc, argv, Options.get(), Buffer.get());
 
   if (Parse.error())
     return 128;
 
   for (option::Option *Opt = Options[UNKNOWN]; Opt; Opt = Opt->next())
-    std::cout << "Unknown options: " <<
-      std::string(Opt->name, Opt->namelen) << std::endl;
+    std::cout << "Unknown options: " << std::string(Opt->name, Opt->namelen)
+              << std::endl;
   if (Options[UNKNOWN])
     return 128;
 
@@ -53,8 +48,6 @@ int main(int argc, char *argv[]) {
     std::string InStream = Parse.nonOption(0);
     ParseFacade Pf(InStream, std::cerr, Options[DEBUG]);
     auto FHandle = Pf.jitAction(ParseSource::STRING, PostParseAction::LLDUMP);
-    delete[] Options;
-    delete[] Buffer;
     FHandle();
     return 0;
   }
@@ -67,8 +60,6 @@ int main(int argc, char *argv[]) {
   std::string InFile = Parse.nonOption(0);
   ParseFacade Pf(InFile, std::cerr, Options[DEBUG]);
   auto FHandle = Pf.jitAction(ParseSource::FILE, PostParseAction::LLDUMP);
-  delete[] Options;
-  delete[] Buffer;
   FHandle();
   return 0;
 }
