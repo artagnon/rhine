5f689a1c7cab7dd28f2e088f2b66057b634a9415
Ramkumar Ramachandra <artagnon@gmail.com>
2015-11-21 18:56:00 -0500

    Wire up dead FunctionTest

--- unittest/FunctionTest.cpp
+++ unittest/FunctionTest.cpp
@@ -3,38 +3,40 @@
 
 using namespace rhine;
 
-TEST(Function, MultipleDefun)
-{
-  auto SourcePrg =
-    "def foo do\n"
-    "  ret 2;\n"
-    "end\n"
-    "def bar do\n"
-    "   ret 3;\n"
-    "end";
+TEST(Function, MultipleDefun) {
+  auto SourcePrg = "def foo do\n"
+                   "  ret 2;\n"
+                   "end\n"
+                   "def bar do\n"
+                   "   ret 3;\n"
+                   "end";
   EXPECT_LL(SourcePrg, "define i32 @foo()", "define i32 @bar()");
 }
 
 TEST(Function, FunctionArgument) {
-  auto SourcePrg =
-    "def printid [var ~String] {\n"
-    "  print var;\n"
-    "}\n"
-    "def main [] {\n"
-    "  printid '21';\n"
-    "}";
-  auto ExpectedOut = "21";
-  EXPECT_OUTPUT(SourcePrg, ExpectedOut);
+  auto SourcePrg = "def printid (var ~String) do\n"
+                   "  print var;\n"
+                   "end\n"
+                   "def main do\n"
+                   "  printid '21';\n"
+                   "end";
+  EXPECT_OUTPUT(SourcePrg, "21");
 }
 
 TEST(Function, VoidArgument) {
-  auto SourcePrg =
-    "def printSomething [] {\n"
-    "  print '27';\n"
-    "}\n"
-    "def main [] {\n"
-    "  printSomething ();\n"
-    "}";
-  auto ExpectedOut = "27";
-  EXPECT_OUTPUT(SourcePrg, ExpectedOut);
+  auto SourcePrg = "def printSomething do\n"
+                   "  print '27';\n"
+                   "end\n"
+                   "def main do\n"
+                   "  printSomething ();\n"
+                   "end";
+  EXPECT_LL(SourcePrg, "call void @printSomething()");
+  EXPECT_OUTPUT(SourcePrg, "27");
+}
+
+TEST(CodeGen, VoidReturn) {
+  auto SourcePrg = "def id do\n"
+                   "  ret ();\n"
+                   "end";
+  EXPECT_LL(SourcePrg, "ret void");
 }
