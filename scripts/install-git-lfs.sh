a47e42246ab16e607783dbb010e75b8b4247782d
Ramkumar Ramachandra <artagnon@gmail.com>
2016-03-30 21:53:16 -0400

    Diagnose the lfs step yet again

--- scripts/install-git-lfs.sh
+++ scripts/install-git-lfs.sh
@@ -5,5 +5,7 @@ if [ $TRAVIS_OS_NAME == linux ] || [ "$(expr substr $(uname -s) 1 5)" == "Linux"
   tar xf git-lfs-linux-amd64-1.1.2.tar.gz &&
   export PATH=`pwd`/git-lfs-1.1.2:$PATH &&
   git lfs fetch &&
-  git checkout -f # old git errors on un-executable smudge/clean filters
+  pwd &&
+  file cmake-ninja-flex.tar.bz2 &&
+  git checkout -f
 fi
