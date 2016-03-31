7cef21627417d1f4abfe70235465cfb355f54bb2
Ramkumar Ramachandra <artagnon@gmail.com>
2016-03-30 20:36:03 -0400

    Fix a couple of Travis issues

--- scripts/install-cmake-ninja-flex.sh
+++ scripts/install-cmake-ninja-flex.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 if [ $TRAVIS_OS_NAME == linux ] || [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  file cmake-ninja-flex.tar.bz2 &&
-  pwd &&
   tar xf cmake-ninja-flex.tar.bz2 &&
   export TOOLS_ROOT=`pwd`/cmake-ninja-flex &&
   export PATH=$TOOLS_ROOT:$PATH
--- scripts/install-git-lfs.sh
+++ scripts/install-git-lfs.sh
@@ -4,5 +4,6 @@ if [ $TRAVIS_OS_NAME == linux ] || [ "$(expr substr $(uname -s) 1 5)" == "Linux"
   wget https://github.com/github/git-lfs/releases/download/v1.1.2/git-lfs-linux-amd64-1.1.2.tar.gz &&
   tar xf git-lfs-linux-amd64-1.1.2.tar.gz &&
   export PATH=`pwd`/git-lfs-1.1.2:$PATH &&
+  git lfs fetch &&
   git checkout -f # old git errors on un-executable smudge/clean filters
 fi
