6e9877d88f43d0b16397079b056c87a838985d06
Ramkumar Ramachandra <artagnon@gmail.com>
2016-07-03 15:42:50 -0400

    Fix coverage-related stuff, need to test-green

--- scripts/install-cmake-ninja-flex.sh
+++ scripts/install-cmake-ninja-flex.sh
@@ -6,6 +6,6 @@ if [ $TRAVIS_OS_NAME == linux ] || [ "$(expr substr $(uname -s) 1 5)" == "Linux"
   export PATH=$TOOLS_ROOT:$PATH
 elif [ $TRAVIS_OS_NAME == osx ] || [ "$(uname)" == "Darwin" ]; then
   brew update &&
-  brew install flex ninja &&
+  brew install cmake ninja flex &&
   brew link --force flex
 fi
